639b145673b6e83b763449caf52b86218004b424
SPUKeenan <lesliek@spu.edu>
2020-02-08 20:21:21 -0800

    Levis updates and designq
    Worked on the design and it looks super close to an Iphone. Also pulled from levis branch

---
+++ ui_mainwindow.h
@@ -0,0 +1,299 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *button2;
+    QPushButton *button9;
+    QPushButton *button8;
+    QPushButton *button7;
+    QPushButton *button6;
+    QPushButton *button5;
+    QPushButton *button4;
+    QPushButton *button3;
+    QPushButton *button1;
+    QPushButton *callbutton;
+    QPushButton *poundbutton;
+    QPushButton *button0;
+    QPushButton *starbutton;
+    QLineEdit *display;
+    QPushButton *Backspace;
+    QPushButton *OpenAddressBook;
+    QTableView *tableView;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(901, 535);
+        MainWindow->setStyleSheet(QString::fromUtf8("	background-color: rgb(9, 8, 8);\n"
+""));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        button2 = new QPushButton(centralwidget);
+        button2->setObjectName(QString::fromUtf8("button2"));
+        button2->setGeometry(QRect(190, 70, 70, 70));
+        QFont font;
+        font.setPointSize(10);
+        button2->setFont(font);
+        button2->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button9 = new QPushButton(centralwidget);
+        button9->setObjectName(QString::fromUtf8("button9"));
+        button9->setGeometry(QRect(270, 230, 70, 70));
+        button9->setFont(font);
+        button9->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button8 = new QPushButton(centralwidget);
+        button8->setObjectName(QString::fromUtf8("button8"));
+        button8->setGeometry(QRect(190, 230, 70, 70));
+        button8->setFont(font);
+        button8->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button7 = new QPushButton(centralwidget);
+        button7->setObjectName(QString::fromUtf8("button7"));
+        button7->setGeometry(QRect(110, 230, 70, 70));
+        button7->setFont(font);
+        button7->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button6 = new QPushButton(centralwidget);
+        button6->setObjectName(QString::fromUtf8("button6"));
+        button6->setGeometry(QRect(270, 150, 70, 70));
+        button6->setFont(font);
+        button6->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button5 = new QPushButton(centralwidget);
+        button5->setObjectName(QString::fromUtf8("button5"));
+        button5->setGeometry(QRect(190, 150, 70, 70));
+        button5->setFont(font);
+        button5->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button4 = new QPushButton(centralwidget);
+        button4->setObjectName(QString::fromUtf8("button4"));
+        button4->setGeometry(QRect(110, 150, 70, 70));
+        button4->setFont(font);
+        button4->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button3 = new QPushButton(centralwidget);
+        button3->setObjectName(QString::fromUtf8("button3"));
+        button3->setGeometry(QRect(270, 70, 70, 70));
+        button3->setFont(font);
+        button3->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button1 = new QPushButton(centralwidget);
+        button1->setObjectName(QString::fromUtf8("button1"));
+        button1->setGeometry(QRect(110, 70, 70, 70));
+        button1->setFont(font);
+        button1->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        callbutton = new QPushButton(centralwidget);
+        callbutton->setObjectName(QString::fromUtf8("callbutton"));
+        callbutton->setGeometry(QRect(190, 390, 70, 70));
+        callbutton->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"{\n"
+"	background-color: rgb(83, 215, 80);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"	image: url(:/new/prefix1/call-answer.png);\n"
+"}\n"
+""));
+        poundbutton = new QPushButton(centralwidget);
+        poundbutton->setObjectName(QString::fromUtf8("poundbutton"));
+        poundbutton->setGeometry(QRect(270, 310, 70, 70));
+        QFont font1;
+        font1.setPointSize(15);
+        poundbutton->setFont(font1);
+        poundbutton->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        button0 = new QPushButton(centralwidget);
+        button0->setObjectName(QString::fromUtf8("button0"));
+        button0->setGeometry(QRect(190, 310, 70, 70));
+        button0->setFont(font);
+        button0->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        starbutton = new QPushButton(centralwidget);
+        starbutton->setObjectName(QString::fromUtf8("starbutton"));
+        starbutton->setGeometry(QRect(110, 310, 70, 70));
+        starbutton->setFont(font1);
+        starbutton->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	border-radius: 35%;\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        display = new QLineEdit(centralwidget);
+        display->setObjectName(QString::fromUtf8("display"));
+        display->setGeometry(QRect(110, 29, 230, 31));
+        display->setStyleSheet(QString::fromUtf8("	color:rgb(255, 255, 255);\n"
+"border: 2px solid #2F3230;\n"
+""));
+        display->setMaxLength(18);
+        display->setFrame(true);
+        Backspace = new QPushButton(centralwidget);
+        Backspace->setObjectName(QString::fromUtf8("Backspace"));
+        Backspace->setGeometry(QRect(350, 30, 80, 31));
+        Backspace->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        OpenAddressBook = new QPushButton(centralwidget);
+        OpenAddressBook->setObjectName(QString::fromUtf8("OpenAddressBook"));
+        OpenAddressBook->setGeometry(QRect(290, 410, 80, 31));
+        OpenAddressBook->setStyleSheet(QString::fromUtf8("QPushButton\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	color:rgb(255, 255, 255);\n"
+"}"));
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+        tableView->setGeometry(QRect(460, 30, 330, 431));
+        tableView->setStyleSheet(QString::fromUtf8("QTableView\n"
+"\n"
+"{\n"
+"	background-color: rgb(47, 50, 48);\n"
+"	color:rgb(255, 255, 255);\n"
+"border: 2px solid #2F3230;\n"
+"}"));
+        tableView->setShowGrid(true);
+        tableView->setGridStyle(Qt::SolidLine);
+        tableView->horizontalHeader()->setVisible(false);
+        tableView->horizontalHeader()->setDefaultSectionSize(103);
+        tableView->verticalHeader()->setVisible(false);
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 901, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        button2->setText(QCoreApplication::translate("MainWindow", "2\n"
+"A B C", nullptr));
+        button9->setText(QCoreApplication::translate("MainWindow", "9\n"
+"W X Y Z", nullptr));
+        button8->setText(QCoreApplication::translate("MainWindow", "8\n"
+"T U V", nullptr));
+        button7->setText(QCoreApplication::translate("MainWindow", "7\n"
+"P Q R S", nullptr));
+        button6->setText(QCoreApplication::translate("MainWindow", "6\n"
+"M N O", nullptr));
+        button5->setText(QCoreApplication::translate("MainWindow", "5\n"
+"J K L", nullptr));
+        button4->setText(QCoreApplication::translate("MainWindow", "4\n"
+"G H I", nullptr));
+        button3->setText(QCoreApplication::translate("MainWindow", "3\n"
+"D E F", nullptr));
+        button1->setText(QCoreApplication::translate("MainWindow", "1\n"
+"", nullptr));
+        callbutton->setText(QString());
+        poundbutton->setText(QCoreApplication::translate("MainWindow", "#", nullptr));
+        button0->setText(QCoreApplication::translate("MainWindow", "0\n"
+"", nullptr));
+        starbutton->setText(QCoreApplication::translate("MainWindow", "*", nullptr));
+        display->setText(QString());
+        Backspace->setText(QCoreApplication::translate("MainWindow", "Delete", nullptr));
+        OpenAddressBook->setText(QCoreApplication::translate("MainWindow", "Contact Book", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
